aa3d6bea70e6aa19b98c5da8548cb6c8d8d209ce
rick951 <rickning@pacificamerican.org>
2021-08-19 14:55:18 +0800

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: ", days)
+print ("Here are the months: ", months)
+
+print("""
+there's something going on here. 
+With the three double-quotes. 
+We'll be able to type as much as we like. 
+Even 4 line if we want, or 5, or 6.
+""")
+#mistake is no () in the code
